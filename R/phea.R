966c65564cf8f90e4d98df8523a5be87be5ac491
Fabrício Kury <fab@kury.dev>
2022-11-20 20:46:06 -0500

    Atlz

--- R/phea.R
+++ R/phea.R
@@ -1,11 +1,13 @@
 # phea.R ----------------------------------------------------------------------------------------------------------
-#' @title phe2a
+#' @title phea
 #' ---
 #' PHEnotyping Algebra
 #' By Fabrício Kury, fab at kury.dev.
 #' 2022-11-17 19:07 EST
 #'
 
+
+# Data ------------------------------------------------------------------------------------------------------------
 .pkgglobalenv <- new.env(parent=emptyenv())
 
 
@@ -13,9 +15,21 @@
 # Declare function colnames_dbplyr(records).
 colnames_dbplyr <- function(records) {
   records |>
-    head(1) |> # Coletar 1 line.
-    collect() |>
+    utils::head(1) |> # Coletar 1 line.
+    dplyr::collect() |>
     colnames()
+}
+
+#' @export
+groupwise_row_filter <- function(lazy_tbl, val_col, partition_cols, win_fn = 'min') {
+  partition_txt <- paste0(partition_cols, collapse = '", "')
+  val_col_name <- deparse(substitute(val_col))
+  sql_txt <- paste0(win_fn, '("', val_col_name, '") over (partition by "', partition_txt, '")')
+  lazy_tbl |>
+    dplyr::mutate(
+      phea_calc_var = dplyr::sql(sql_txt)) |>
+    dplyr::filter(!!rlang::sym(val_col_name) == phea_calc_var) |>
+    dplyr::select(-phea_calc_var)
 }
 
 #' @export
@@ -26,16 +40,16 @@ setup_phea <- function(dbi_connection, schema) {
 
 #' @export
 sqlt <- function(table) {
-  tbl(.pkgglobalenv$con,
-    in_schema(.pkgglobalenv$schema,
+  dplyr::tbl(.pkgglobalenv$con,
+    dbplyr::in_schema(.pkgglobalenv$schema,
       deparse(substitute(table))))
 }
 
 #' @export
 sql0 <- function(...) {
   sql_txt <- paste0(...)
-  tbl(.pkgglobalenv$con,
-    sql(sql_txt))
+  dplyr::tbl(.pkgglobalenv$con,
+    dplyr::sql(sql_txt))
 }
 
 #' @export
@@ -95,8 +109,6 @@ make_component <- function(rec_source, line = NA, delay = NA, window = Inf, rec_
 
 #' make_record_source
 #'
-#' This function...
-#'
 #' @param records List of records.
 #' @param rec_name Record name.
 #' @return Record source..
@@ -117,7 +129,7 @@ make_record_source <- function(records, rec_name, ts, pid, vars = NULL, .capture
 
     if(is.null(vars)) {
       vars <- records |>
-        select(!!rec_name_name) |> distinct() |> collect() |> pull()
+        dplyr::select(!!rlang::sym(rec_name_name)) |> dplyr::distinct() |> dplyr::collect() |> dplyr::pull()
     }
 
   } else {
@@ -148,21 +160,18 @@ make_record_source <- function(records, rec_name, ts, pid, vars = NULL, .capture
 
 #' Calculate formula
 #'
-#' This function gathers records according to their timestamps and computes a SQL formula.
+#' Gathers records according to their timestamps and computes a SQL formula.
 #'
+#' @export
 #' @param components List of components,  component, record source, or lazy table.
 #' @param fml Formula or list of formulas.
 #' @param export List of additional variables to export.
 #' @param add_components Additional components to add in case components is not a list of components.
 #' @return Lazy table with result of formula or formulas.
-#' @export
-#' @examples
-#' teste
 calculate_formula <- function(components, fml = NULL, window = NA, export = NULL, add_components = NULL,
+  # These below are used to provide a lazy table directly, instead of components.
   .ts = NULL, .pid = NULL, .rec_name = NULL, .delay = NULL, .line = NULL,
-  # These are used to provide a lazy table directly, instead of components.
-  .ordem = NULL, .exclui_na = FALSE, .require_all = FALSE, .lim = NA, .dont_require = NULL,
-  .cascaded = FALSE) {
+  .exclude_na = FALSE, .require_all = FALSE, .lim = NA, .dont_require = NULL, .cascaded = FALSE) {
   # Prepare ---------------------------------------------------------------------------------------------------------
   if('tbl_lazy' %in% class(components)) {
     #' components is actually a lazy table. Make a component out of it. The function make_component() is also overloaded
@@ -197,9 +206,9 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     components <- list(components)
 
   # Build record sources
-  rec_source_names <- map(components, ~.$rec_source$rec_name) |> unlist()
+  rec_source_names <- purrr::map(components, ~.$rec_source$rec_name) |> unlist()
   rec_source_mask <- !duplicated(rec_source_names)
-  record_sources <- map(components[rec_source_mask], ~.$rec_source)
+  record_sources <- purrr::map(components[rec_source_mask], ~.$rec_source)
 
   if(is.null(names(components)) &&
       isTRUE(attr(components[[1]], 'phea') == 'component')) {
@@ -217,20 +226,20 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     }
 
     # Extract used components from formula.
-    g_vars <- str_match_all(fml, '([A-z][A-z0-9_]+)') |> reduce(rbind)
+    g_vars <- stringr::str_match_all(fml, '([A-z][A-z0-9_]+)') |> purrr::reduce(rbind)
     g_vars <- g_vars[,2]
 
     #' Filter bogus matches (eg. SQL keywords in the formula) by keeping only the variables that can possibly come from
     #' the given combination of record sources and components.
-    out_g_vars_mask <- map(record_sources, \(record_source) {
+    out_g_vars_mask <- purrr::map(record_sources, \(record_source) {
       vars <- record_source$vars
-      comp_mask <- map(components, ~.$rec_source$rec_name) == record_source$rec_name
+      comp_mask <- purrr::map(components, ~.$rec_source$rec_name) == record_source$rec_name
       related_components <- names(components)[comp_mask]
-      pairs <- cross2(related_components, vars) # Create all combinations of related components and rec_source's vars.
-      out_vars <- map(pairs, \(x) paste0(x[[1]], '_', x[[2]])) |> unlist()
+      pairs <- purrr::cross2(related_components, vars) # Create all combinations of components and vars.
+      out_vars <- purrr::map(pairs, \(x) paste0(x[[1]], '_', x[[2]])) |> unlist()
       out_vars_mask <- g_vars %in% out_vars
       return(out_vars_mask)
-    }) |> reduce(`|`) # Do element-wise OR operation. Combine with OR.
+    }) |> purrr::reduce(`|`) # Do element-wise OR operation. Combine with OR.
 
     g_vars <- g_vars[out_g_vars_mask]
   } else {
@@ -255,46 +264,73 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     if(rec_type == 'column') {
       capture_col <- record_source$capture_col
       sql_txt <- paste0("concat('", rec_name, "_', \"", rec_name, '")')
-      export_records <- transmute(record_source$records,
-        name = sql(sql_txt),
-        pid = !!sym(rec_pid),
-        ts = !!sym(ts),
-        capture_col = !!sym(capture_col))
+      export_records <- dplyr::transmute(record_source$records,
+        name = dplyr::sql(sql_txt),
+        pid = !!rlang::sym(rec_pid),
+        ts = !!rlang::sym(ts),
+        capture_col = !!rlang::sym(capture_col))
     } else {
-      export_records <- mutate(record_source$records,
+      export_records <- dplyr::mutate(record_source$records,
         name = local(rec_name),
-        pid = !!sym(rec_pid),
-        ts = !!sym(ts))
+        pid = !!rlang::sym(rec_pid),
+        ts = !!rlang::sym(ts))
 
       #' Select only the columns that will be needed later, according to g_vars. This requires checking all applicable
       #' components.
       vars <- record_source$vars
-      comp_mask <- map(components, ~.$rec_source$rec_name) == rec_name
+      comp_mask <- purrr::map(components, ~.$rec_source$rec_name) == rec_name
       related_components <- names(components)[comp_mask]
-      pairs <- cross2(related_components, vars) # Create all combinations of related components and rec_source's vars.
-      pairs_mask <- map(pairs, \(x) paste0(x[[1]], '_', x[[2]]) %in% g_vars) |> unlist()
+      pairs <- purrr::cross2(related_components, vars) # Create all combinations of related components and rec_source's vars.
+      pairs_mask <- purrr::map(pairs, \(x) paste0(x[[1]], '_', x[[2]]) %in% g_vars) |> unlist()
 
       if(any(pairs_mask)) {
         out_vars <- pairs[unlist(pairs_mask)] |>
-          map(~.[[2]]) |> unlist()
+          purrr::map(~.[[2]]) |> unlist()
 
         export_records <- export_records |>
-          select(name, pid, ts,
+          dplyr::select(name, pid, ts,
             all_of(out_vars))
       } else {
         export_records <- export_records |>
-          select(name, pid, ts)
+          dplyr::select(name, pid, ts)
       }
     }
 
     return(export_records)
   }
 
-  board <- map(record_sources, prepare_record_source) |>
-    reduce(union_all)
+  board <- purrr::map(record_sources, prepare_record_source) |>
+    purrr::reduce(dplyr::union_all)
 
   # Criar colunas dos components -----------------------------------------------------------------------------------
-  # Primeiro, gerar os comandos.
+  extract_vars <- function(comp_name, component) {
+    rec_name <- component$rec_source$rec_name
+    # Locate the record source, so we know what variables to extract.
+    rec_mask <- purrr::map(record_sources, ~.$rec_name) == rec_name
+    rec_vars <- purrr::map(record_sources[rec_mask], \(rec_source) {
+      if(rec_source$type == 'direct')
+        return(rec_source$vars)
+      else {
+        rec_vars <- rec_source$records |>
+          dplyr::select(!!rlang::sym(rec_source$rec_name)) |>
+          dplyr::distinct() |>
+          dplyr::pull()
+        rec_vars <- paste0(rec_source$rec_name, '_', rec_vars)
+        return(rec_vars)
+      }
+    }) |> unlist()
+
+    if(component$rec_source$type == 'direct') {
+      vars <- c(rec_vars, 'ts')
+      ts_var <- paste0(comp_name, '_ts')
+      vars_mask <- paste0(comp_name, '_', vars) %in% c(g_vars, ts_var)
+    } else {
+      vars <- unlist(rec_vars)
+      vars_mask <- vars %in% g_vars
+    }
+    vars[vars_mask]
+  }
+
   produce_component <- function(comp_name, component) {
     rec_name <- component$rec_source$rec_name
 
@@ -302,35 +338,12 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     line <- component$line
     comp_window <- component$comp_window
 
-    # Locate the record source, so we know what variables to extract.
-    rec_mask <- map(record_sources, ~.$rec_name) == rec_name
-    rec_vars <- map(record_sources[rec_mask], \(rec_source) {
-      if(rec_source$type == 'direct')
-        return(rec_source$vars)
-      else {
-        rec_vars <- rec_source$records |>
-          select(!!sym(rec_source$rec_name)) |>
-          distinct() |>
-          pull()
-        rec_vars <- paste0(rec_source$rec_name, '_', rec_vars)
-        return(rec_vars)
-      }
-    })
+    vars <- extract_vars(comp_name, component)
 
-    if(component$rec_source$type == 'direct') {
-      vars <- c(rec_vars, 'ts')
-      ts_var <- paste0(comp_name, '_ts')
-      vars_mask <- paste0(comp_name, '_', vars) %in% c(g_vars, ts_var)
-      vars <- vars[vars_mask]
+    if(component$rec_source$type == 'direct')
       colunas <- paste0(comp_name, '_', vars, '_', calc_suffix)
-    } else {
-      vars <- unlist(rec_vars)
-      ts_vars <- paste0(vars, '_ts')
-      vars <- c(vars, ts_vars)
-      vars_mask <- vars %in% g_vars
-      vars <- vars[vars_mask]
+    else
       colunas <- paste0(vars, '_', calc_suffix)
-    }
 
     #' Por algum motivo, o "lag()" não funciona com "range between window preceeding and delay preceeding", i.e. não é
     #' possível aplicar line *e* delay em uma só chamada à função de window. O last_value() funciona com esse "range".
@@ -358,53 +371,26 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
         range_start_clause, ' and \'', delay, '\'::interval preceding', ')')
     }
 
-    comandos <- map2(colunas, sql_txts,
-      ~rlang::exprs(!!..1 := sql(!!..2))) |>
+    comandos <- purrr::map2(colunas, sql_txts,
+      ~rlang::exprs(!!..1 := dplyr::sql(!!..2))) |>
       unlist()
 
     return(comandos)
   }
 
-  commands <- map2(names(components), components, produce_component) |> unlist()
+  # First, generate commands.
+  commands <- purrr::map2(names(components), components, produce_component) |> unlist()
 
-  # Então, aplicá-los na board.
-  board <- transmute(board,
+  # Then, apply on the board.
+  board <- dplyr::transmute(board,
     pid, ts,
-    row_id = sql('row_number() over ()'),
+    row_id = dplyr::sql('row_number() over ()'),
     !!!commands)
 
   # Preencher os valores em branco. ---------------------------------------------------------------------------------
   #' Neste ponto o componente já foi adicionado à board de valores, porém cada componente só tem valor em sua line de
   #' origem. Se o PostgreSQL suportasse IGNORE NULLS no last_value(), o trabalho terminaria aqui. Como ele não
   #' suporta, precisamos contornar o problema.
-  extract_vars <- function(comp_name, component) {
-    rec_name <- component$rec_source$rec_name
-    # Locate the record source, so we know what variables to extract.
-    rec_mask <- map(record_sources, ~.$rec_name) == rec_name
-    rec_vars <- map(record_sources[rec_mask], \(rec_source) {
-      if(rec_source$type == 'direct')
-        return(rec_source$vars)
-      else {
-        rec_vars <- rec_source$records |>
-          select(!!sym(rec_source$rec_name)) |>
-          distinct() |>
-          pull()
-        rec_vars <- paste0(rec_source$rec_name, '_', rec_vars)
-        return(rec_vars)
-      }
-    })
-
-    if(component$rec_source$type == 'direct') {
-      vars <- c(rec_vars, 'ts')
-      ts_var <- paste0(comp_name, '_ts')
-      vars_mask <- paste0(comp_name, '_', vars) %in% c(g_vars, ts_var)
-    } else {
-      vars <- unlist(rec_vars)
-      vars_mask <- vars %in% g_vars
-    }
-    vars[vars_mask]
-  }
-
   extract_ts_variables <- function(i) {
     component <- components[[i]]
     if(component$rec_source$type == 'direct') {
@@ -415,35 +401,35 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     }
   }
 
-  ts_variables <- map(seq(components), extract_ts_variables) |> unlist()
-# browser()
+  ts_variables <- purrr::map(seq(components), extract_ts_variables) |> unlist()
+
   calc_vars <- unique(c(g_vars, ts_variables))
   sql_txts <- paste0('find_last_ignore_nulls("', calc_vars, '_', calc_suffix, '") OVER ',
     '(PARTITION BY "pid" ORDER BY "ts" ROWS UNBOUNDED PRECEDING)')
 
-  comandos <- map2(calc_vars, sql_txts,
-    ~rlang::exprs(!!.x := sql(!!.y))) |>
+  commands <- purrr::map2(calc_vars, sql_txts,
+    ~rlang::exprs(!!.x := dplyr::sql(!!.y))) |>
     unlist()
 
   #' The front (most recent point) of the window is column ts of the current line. The back (oldest point) is the
   #' smallest among the ts's of the components.
-  sql_datahora_min <- paste0('least(', paste0(ts_variables, collapse = ', '), ')')
+  sql_ts_least <- paste0('least(', paste0(ts_variables, collapse = ', '), ')')
 
   # Apply the commands.
   #' The use of transmute + mutate + select(-...) is to keep the query "tight." By "tight" I mean no use of select *,
   #' that is, every step of the process passes forward only strictly what the next step needs. The hope is that this
   #' will help the SQL query optimizer and computation engine as a whole.
   board <- board |>
-    transmute(
+    dplyr::transmute(
       row_id, pid, ts,
-      !!!comandos,
-      window = ts - sql(sql_datahora_min),
-      ts_row = sql('last_value(row_id) over (partition by "pid", "ts")'))
+      !!!commands,
+      window = ts - dplyr::sql(sql_ts_least),
+      ts_row = dplyr::sql('last_value(row_id) over (partition by "pid", "ts")'))
 
   #' Keep only the most complete computation in each timestamp. The most recent computation is the last one in each
   #' timestamp. 'max(row_id) over (partition by "pid", "ts")' could find the row with the largest (most complete) row_id
   #' in each timestamp, but last_value() in this context gives the same result
-  #' We also need to potentially require all fields be filled. Let's compact that into a single call to filter(), hence
+  #' We also need to potentially require all fields be filled. Let's compact that into a single call to dplyr::filter(), hence
   #' a single WHERE statement.
   if(.require_all || !is.null(.dont_require)) {
     #' If .dont_require is provided, then all components, except those specified, will be required, even if .require_all
@@ -451,34 +437,34 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
     required_components <- setdiff(names(components), .dont_require)
     if(length(required_components) > 0) {
       sql_txt <- paste0(required_components, '_ts is not null') |> paste(collapse = ' and ')
-      board <- filter(board,
-        row_id == ts_row && sql(sql_txt))
+      board <- dplyr::filter(board,
+        row_id == ts_row && dplyr::sql(sql_txt))
     } else {
       # No required components after all, because all were excluded by .dont_require.
-      board <- filter(board,
+      board <- dplyr::filter(board,
         row_id == ts_row)
     }
   } else {
     board <- board |>
-      filter(row_id == ts_row)
+      dplyr::filter(row_id == ts_row)
   }
 
   # Clean temporary variables used for computing the formula.
   board <- board |>
-    select(
+    dplyr::select(
       -ts_row,
-      -any_of(setdiff(calc_vars, g_vars)))
+      -dplyr::any_of(setdiff(calc_vars, g_vars)))
 
   # Filtrar e calcular ----------------------------------------------------------------------------------------------
   # Impose the time window, if any.
   if(!is.na(window))
-    board <- filter(board,
+    board <- dplyr::filter(board,
       window < local(window))
 
   # Limit rows.
   if(!is.na(.lim))
     board <- board |>
-    head(n = lim)
+      head(n = lim)
 
   # Calculate the formula, if any.
   if(!is.null(fml)) {
@@ -486,29 +472,29 @@ calculate_formula <- function(components, fml = NULL, window = NA, export = NULL
       # Compute one at a time, so that the prior result can be used in the next formula.
       for(i in seq(fml)) {
         sql_txt <- fml[[i]]
-        board <- mutate(board,
-          !!sym(names(fml)[i]) := sql(sql_txt))
+        board <- dplyr::mutate(board,
+          !!rlang::sym(names(fml)[i]) := dplyr::sql(sql_txt))
       }
     } else {
       # Compute them all in one statement, so that computation time is (potentially, haven't tested) minimized.
-      commands <- map2(names(fml), fml,
-        ~rlang::exprs(!!..1 := sql(!!..2))) |>
+      commands <- purrr::map2(names(fml), fml,
+        ~rlang::exprs(!!..1 := dplyr::sql(!!..2))) |>
         unlist()
 
-      board <- mutate(board,
+      board <- dplyr::mutate(board,
         !!!commands)
     }
   }
 
-  # Requerer que o resultado exista.
-  if(.exclui_na)
-    board <- filter(board,
-      !is.na(!!sym(.out_var)))
+  # Require result to exist.
+  if(.exclude_na)
+    board <- dplyr::filter(board,
+      !is.na(!!rlang::sym(.out_var)))
 
   # Colapsar e retornar -------------------------------------------------------------------------------------------
   # Chamar o collapse() é necessário para evitar acúmulo de código lazy com eventual erro "C stack usage is too close to
   # the limit". Vide https://github.com/tidyverse/dbplyr/issues/719. Obrigado, mgirlich!
-  board <- collapse(board, cte = TRUE)
+  board <- dplyr::collapse(board, cte = TRUE)
 
   board
 }
