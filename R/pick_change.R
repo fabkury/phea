c8b82486aa70dd2f7bca62cbf35a162f6df2756f
Fabrício Kury <fab@kury.dev>
2022-12-26 21:41:23 -0500

    0.6.3

--- R/pick_change.R
+++ R/pick_change.R
@@ -7,55 +7,60 @@
 # Pick row by & keep change of ------------------------------------------------------------------------------------
 #' Pick row by window function
 #'
-#' Pick (keep) the row(s) that contain the group-wise aggregate value of `.fn` in each partition. If `.fn` is not
-#' provided, defaults to picking the rows where the value of `by` is minimum.
-#'
-#' If `pick_min` is `TRUE`, defaults instead to rows where `by` is maximum.
+#' Pick (keep) the row(s) that contain the frame-wise aggregate value of window function `fn`. If `fn` is not provided,
+#' defaults to picking the rows where the value of `by` is minimum (i.e. first row in ascending order). If `pick_max` is
+#' `TRUE`, defaults instead to rows where `by` is maximum (i.e. last row in ascending order).
 #' 
-#' If `.val` is provided, keeps the rows where the result of `.fn(by)` is equal to `.val`.
+#' If `val` is provided, keeps the rows where the result of `fn(by)` is equal to `val` (if any).
+#' 
+#' If `partition` is provided, it is used when calling the window function.
 #'
 #' @export
 #' @param lazy_tbl Lazy table to be filtered.
 #' @param by Character. Column(s) to pick rows by.
 #' @param partition Character vector. Column name(s) to define the partition.
-#' @param pick_max Logical. If `TRUE`, will pick the last row, instead of first.
-#' @param .fn Character. Name of the aggregate function to use, *without parentheses*. E.g.: `max`, `cume_dist`.
-#' @param .val Character or numeric. Literal value to compare to result of `.fn`.
+#' @param pick_max Logical. If `TRUE`, will pick the row with largest `by`, instead of smallest.
+#' @param fn Character. Window function to use, with arguments if needed. E.g.: `row_number()`,
+#' `perc_dist(0.5)`.
+#' @param val Character or numeric. Literal value to compare to result of `.fn`.
 #' @return Lazy table with filtered rows.
-pick_row_by <- function(lazy_tbl, by, partition, pick_max = FALSE, .fn = NULL, .fn_arg = NULL, .val = NULL) {
-  if(is.null(.fn)) {
+pick_row_by <- function(lazy_tbl, by, partition = NULL, pick_max = FALSE, fn = NULL, val = NULL) {
+  if(is.null(fn)) {
     # The default operation is done using `row_number() == 1`/`cume_dist() == 1`, as opposed to `max()`/`min()`, to
     # spare the need to create a new variable (column) to perform the computation, since window functions cannot go in
     # `WHERE` clauses.
     if(pick_max)
-      .fn <- 'cume_dist'
+      fn <- 'cume_dist()'
     else
-      .fn <- 'row_number'
+      fn <- 'row_number()'
     
-    if(is.null(.val))
-      .val <- 1
+    if(is.null(val))
+      val <- 1
   }
   
-  sql_txt <- paste0(.fn, '(', .fn_arg, ') over (partition by "', 
-    paste0(partition, collapse = '", "'), '" order by "', by, '")')
+  # Produce the window function
+  win_sql <- dbplyr::win_over(
+    expr = fn,
+    partition = partition,
+    order = by,
+    con = .pheaglobalenv$con)
   
-  if(is.null(.val)) {
-    # The user provided .fn but not .val. If .fn had _not_ been provided, .val would be non-null.
+  # Calculate the window function.
+  res <- lazy_tbl |>
+    dplyr::mutate(phea_calc_var = win_sql)
+  
+  if(is.null(val)) {
+    # The user provided fn but not val. If fn had _not_ been provided, val would be non-null.
     # This means the user just provided the function name, but no target value. In this case we compare the function
     # result to its input value, .fn(x) == x.
-    res <- lazy_tbl |>
-      dplyr::mutate(
-        phea_calc_var = dplyr::sql(sql_txt)) |>
-      dplyr::filter(!!by == phea_calc_var) |>
-      dplyr::select(-phea_calc_var)
+    res <- dplyr::filter(res, !!by == phea_calc_var)
   } else {
-    # neither .val or .fn are null.
-    res <- lazy_tbl |>
-      dplyr::mutate(
-        phea_calc_var = dplyr::sql(sql_txt)) |>
-      dplyr::filter(phea_calc_var == local(.val)) |>
-      dplyr::select(-phea_calc_var)
+    # val and fn are both non-null.
+    res <- dplyr::filter(res, phea_calc_var == local(val))
   }
+  
+  # Remove field created for the computation.
+  res <- dplyr::select(res, -phea_calc_var)
   
   res
 }
@@ -65,9 +70,8 @@ pick_row_by <- function(lazy_tbl, by, partition, pick_max = FALSE, .fn = NULL, .
 #' Keeps only the rows where the value of `of` changes. `of` can be the name of a column, or any SQL expression valid
 #' inside a `SELECT` statement.
 #' 
-#' If `partition` is provided, changes are limited to within it. If `order` provided, rows
-#' are ordered (and change is detected) according to that column or columns. The first row (within a partition or not)
-#' is always kept.  
+#' If `partition` is provided, changes are limited to within it. If `order` provided, rows are ordered and change is
+#' detected according to the specified column or columns. The first row (within a partition or not) is always kept.  
 #'
 #' @export
 #' @param lazy_tbl Lazy table to be filtered.
@@ -77,19 +81,17 @@ pick_row_by <- function(lazy_tbl, by, partition, pick_max = FALSE, .fn = NULL, .
 #' @param order Character. Optional. If provided, this or these column(s) will define the ordering of the rows and hence
 #' how changes are detected.
 #' @return Lazy table with only rows where `of` changes in comparison to the previous row.
-keep_change_of <- function(lazy_tbl, of, partition = NULL, order = NULL, con = NULL) {
-  if(is.null(con)) {
-    if(is.null(.pheaglobalenv$con))
-      stop('Connection must be provided or setup previously with setup_phea().')
-    else
-      con <- .pheaglobalenv$con
-  }
-
+keep_change_of <- function(lazy_tbl, of, partition = NULL, order = NULL) {
+  if(is.null(.pheaglobalenv$con))
+    stop('Connection must be provided or setup previously with setup_phea().')
+  con <- .pheaglobalenv$con
+  
   if(!is.null(partition))
     of <- c(partition, of)
   
+  of_ids <- DBI::dbQuoteIdentifier(.pheaglobalenv$con, of)
   lag_names <- paste0('phea_kco_lag', seq(of))
-  lag_sql <- paste0('lag(', of, ')')
+  lag_sql <- paste0('lag(', of_ids, ')')
   
   commands_b <- purrr::map2(lag_names, lag_sql, ~rlang::exprs(
     !!..1 := dbplyr::win_over(
@@ -103,10 +105,13 @@ keep_change_of <- function(lazy_tbl, of, partition = NULL, order = NULL, con = N
   # - prior row is different from current row
   # - if there is no prior row, keep current row if current row is not NA
   # - if all rows are NA, nothing will be in the output.
+  # lag_names_ids <- DBI::dbQuoteIdentifier(.pheaglobalenv$con, lag_names)
+  # TODO: Revise if I need to call DBI::dbIdentifierQuote() for lag_names and of in commands_c. This conflicts with the
+  # use of str2lang().
   commands_c <- paste0('(is.na(', lag_names, ') && !is.na(', of, ')) || ', lag_names, ' != ', of) |>
     paste0(collapse = ' || ') |>
     str2lang()
-  
+    
   lazy_tbl |>
     mutate(!!!commands_b) |>
     filter(!!commands_c) |>
--- R/setup.R
+++ R/setup.R
@@ -18,13 +18,13 @@ if(!exists('.pheaglobalenv'))
 #' @export
 #' @param connection DBI-compatible SQL connection (e.g. produced by DBI::dbConnect).
 #' @param schema Schema to be used by default in `sqlt()`. If no schema, use `NA`.
-#' @param .verbose Logical. Optional. If TRUE (default), functions will print to console at times.
+#' @param verbose Logical. Optional. If TRUE (default), functions will print to console at times.
 #' @param .fix_dbplyr_spark Logical. Optional. Very niche functionality. Set to `TRUE` to attempt to fix the use of
 #' `IGNORE NULLS` by the OBDC driver connected to a Spark SQL server/cluster.
-setup_phea <- function(connection, schema, .verbose = TRUE, .fix_dbplyr_spark = FALSE) {
+setup_phea <- function(connection, schema, verbose = TRUE, .fix_dbplyr_spark = FALSE) {
   assign('con', connection, envir = .pheaglobalenv)
   assign('schema', schema, envir = .pheaglobalenv)
-  assign('verbose', .verbose, envir = .pheaglobalenv)
+  assign('verbose', verbose, envir = .pheaglobalenv)
   
   if(.fix_dbplyr_spark) {
     if(connection@info$dbms.name == "Spark SQL") {
